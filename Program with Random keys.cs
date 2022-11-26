bafc94f2753e01f4da4d1d4efaaba9c95b10fb71
TortoisesShell <dylanblues@outlook.com>
2022-11-26 19:59:59 +0200

    Added required files

---
+++ Program with Random keys.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace App
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+
+            // SHARED PUBLIC ELEMENTS
+            Console.WriteLine("Enter q (a prime number): ");
+            long q = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter a (an integer): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+
+            // PRIVATE KEYS
+            Random key = new Random();
+            long xA = key.Next((int) q);
+            long xB = key.Next((int) q);
+
+
+            // PUBLIC KEYS
+            long yA = (long) Math.Pow(a , xA) % q;
+            long yB = (long) Math.Pow(a , xB) % q;
+
+
+            // SHARED KEYS
+            long kAB = (long) Math.Pow(yB , xA) % q;
+            kAB = Math.Abs(kAB);
+
+            long kBA = (long) Math.Pow(yA , xB) % q;
+            kBA = Math.Abs(kBA);
+
+            // CONSOLE OUTPUT
+            Console.WriteLine(kAB);
+            Console.WriteLine(kBA);
+
+        }
+    }
+}
---
+++ Program with user input.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace App
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+
+            // SHARED PUBLIC ELEMENTS
+            Console.WriteLine("Enter q (a prime number): ");
+            ulong q = Convert.ToUInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter a (an integer): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+
+            // PRIVATE KEYS
+            Console.WriteLine("Enter the first private key of A \n Xa: ");
+            ulong xA = Convert.ToUInt64(Console.ReadLine());
+            Console.WriteLine("Xb: ");
+            ulong xB = Convert.ToUInt64(Console.ReadLine());
+
+
+            // PUBLIC KEYS
+            ulong yA = (ulong) Math.Pow(a , xA) % q;
+            Console.WriteLine($"Ya is {yA}");
+
+            ulong yB = (ulong) Math.Pow(a , xB) % q;
+            Console.WriteLine($"Yb is {yB}");
+
+
+            // SHARED KEYS
+            ulong kAB = (ulong) Math.Pow(yB , xA) % q;
+
+            ulong kBA = (ulong) Math.Pow(yA , xB) % q;
+
+            // CONSOLE OUTPUT
+            Console.WriteLine(kAB);
+            Console.WriteLine(kBA);
+
+        }
+    }
+}
